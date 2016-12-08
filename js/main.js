98230277efd3a61d7d71ef5c801b2e8802b8659c
Jesse Clark <wjesseclark@gmail.com>
2016-12-08 18:54:42 -0500

    beginning js-logic-loops

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var owenWilson = 'Wow'; 
+var dharmaInitiative = [4, 8, 15, 16, 23, 42];
+var nicolasCage = true;
+var user = {
+	firstName:'Buster',
+		lastName: 'Bluth'
+};
+var cake = 100;
+console.log(nicolasCage);
+
+console.log(dharmaInitiative[3]);
+
+
+
